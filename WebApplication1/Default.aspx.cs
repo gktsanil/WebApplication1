4b1ae3d3840a286996b699c9e8a3a4f0f3c8ff37
anilgoktas <gkts.anil@gmail.com>
2018-12-06 01:46:14 +0300

    Initial commit

---
+++ WebApplication1/Default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using Amazon;
+using Amazon.S3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebApplication1.Model;
+
+namespace WebApplication1
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnRedirectClick(object sender, EventArgs e)
+        {
+            AmazonS3Client client = new AmazonS3Client("awsAccessKey", "awsScreetKey", RegionEndpoint.APNortheast1);
+        }
+    }
+}
